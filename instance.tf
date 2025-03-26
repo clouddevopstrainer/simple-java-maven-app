76ea6a0edec821926229f2d6a469a059502ea2b7
clouddevopstrainer <clouddevopstrainer@outlook.com>
2025-03-26 12:14:51 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -17,8 +17,9 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  key_name      = aws_key_pair.key_pair.key_name  # Add this line
 
-  tags = {
+tags = {
     Name = "HelloWorld"
   }
 }
