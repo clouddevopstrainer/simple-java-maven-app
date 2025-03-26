d8623de2b8fba262ac041d7490973d54a72437e3
clouddevopstrainer <clouddevopstrainer@outlook.com>
2025-03-26 11:56:32 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,15 +1,24 @@
-resource "aws_instance" "public_instance" {
-  ami                    = "ami-068da067c3c4ef0d3"  
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-  tags = {
-    Name = "public_instance"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
-  root_block_device {
-    volume_size = 30
-    volume_type = "gp2"
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
   }
 }
