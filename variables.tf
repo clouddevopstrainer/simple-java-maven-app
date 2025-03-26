3ceedad61176d1ebae223913308d46c2a90a8ba6
clouddevopstrainer <clouddevopstrainer@outlook.com>
2025-03-26 11:34:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,10 @@ variable "key_name" {
   description = "Name of the SSH key pair"
   default     = "my-key"
 }
+
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
