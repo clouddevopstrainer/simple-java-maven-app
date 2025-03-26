66448b936be5cd309b7c168089f02d32c0726b46
clouddevopstrainer <clouddevopstrainer@outlook.com>
2025-03-26 11:22:48 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "public_instance" {
+  ami                    = ami-0e1bed4f06a3b463d
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+
+  tags = {
+    Name = "public_instance"
+  }
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+}
